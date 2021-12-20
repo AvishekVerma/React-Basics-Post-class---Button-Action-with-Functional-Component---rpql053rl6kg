2930b736c73099423c7958401a5b3161184df7c4
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2021-12-20 13:55:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,9 @@ function App() {
   return (
     <div id="main">
       // Do not alter the main div
+    
+      <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
+    
       <Button_Action />
     </div>
   );
