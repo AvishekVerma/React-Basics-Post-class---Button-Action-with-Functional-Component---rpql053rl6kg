d76ce7085902d26365acf073cc88b9fcaf9b3a3f
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2022-01-05 16:10:02 +0530

    function and contition added

--- src/components/Button_Action.js
+++ src/components/Button_Action.js
@@ -3,16 +3,26 @@ import para from "./para";
 
 
 function Button_Action(){
+    
+    const [state, setState] = useState(false)
+    
     function clickHandler(){
-        return(
-            <div>
-                <para />
-            </div>
-        )
-    }
+     () => setState(true)
+   }    
+//     function clickHandler(){
+//         return(
+//             <div>
+//                 <para />
+//             </div>
+//         )
+//     }
     return(
         <div>
+        
+            //if(state) && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
+             //<button onClick=[() => setState(!true)] id="click">Click</button>
             <button onClick={clickHandler} id="click">Click</button>
+            if(state) && <para />
             
         </div>
     )
