80797dc1f64d3f88165097ef75aa25177f8a23f3
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2022-01-05 16:06:42 +0530

    revert changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Button_Action from "./Button_Action";
 
 function App() {
   
-  const [state, setState] = useState(false)
+  //const [state, setState] = useState(false)
   
 //   function clickHandler(){
 //     () => setState(!true)
@@ -14,10 +14,10 @@ function App() {
     <div id="main">
       // Do not alter the main div
     
-      if(state) && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
+      //if(state) && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
     
-      <button onClick=[() => setState(!true)] id="click">Click</button>
-//       <Button_Action />
+      //<button onClick=[() => setState(!true)] id="click">Click</button>
+      <Button_Action />
     </div>
   );
 }
