fa34ed25fce392306e1ac4deeb7c67edac4a1336
Avishek <avishekverma298@gmail.com>
2022-01-22 21:50:43 +0530

    app file modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 import Button_Action from "./Button_Action";
+import para from "./para";
+
 
 function App() {
   
-  // const [state, setState] = useState(false)
+  const [state, setState] = useState(false)
   
-//   function clickHandler(){
-//     () => setState(!true)
-//   }
+  function clickHandler() {
+    () => setState(true)
+  }
   
   return (
     <div id="main">
       // Do not alter the main div
-      <Button_Action />
+      <button onClick={clickHandler} id="click">Click</button>
+      if(state) && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+            
+      {/* <Button_Action /> */}
     </div>
   );
 }
